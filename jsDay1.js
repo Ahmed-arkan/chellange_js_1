ea082b0976bf374a2d1b867cb094cb109f5550dc
admed-arkan <ahmedarkan@icloud.com>
2017-11-30 08:59:17 +0100

    chellangejs1

---
+++ jsDay1.js
@@ -0,0 +1,14 @@
+
+var Vienna = ["Vienna ","1","2","3","4"];
+var Is =[" 1"," is","2","3","4"];
+var A =["0","1"," a","","4"];
+var Nice =["0","1","2"," nice","4"];
+var City =["0","1","2","3"," city"];
+
+console.log( Vienna[0] + Is[1] + A[2]+ Nice[3] + City[4])
+
+document.getElementById("wien").innerHTML = (Vienna[0] + Is[1] + A[2]+ Nice[3] + City[4]);
+document.getElementById("wien1").innerHTML = (Vienna[0] + Is[1] + A[2]+ Nice[3] + City[4]);
+document.getElementById("wien2").innerHTML = (Vienna[0] + Is[1] + A[2]+ Nice[3] + City[4]);
+document.getElementById("wien3").innerHTML = (Vienna[0] + Is[1] + A[2]+ Nice[3] + City[4]);
+document.getElementById("wien4").innerHTML = (Vienna[0] + Is[1] + A[2]+ Nice[3] + City[4]);
